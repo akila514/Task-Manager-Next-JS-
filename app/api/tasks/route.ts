077c4b899f4c9a129f3c700439033e494bb89f32
akila514 <akilajay514@gmail.com>
2023-11-25 09:55:41 +0530

    Added route to create a task

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,11 +1,17 @@
+import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    console.log(body);
 
-    return NextResponse.json(req);
+    const task = await db.task.create({
+      data: {
+        title: body.title,
+      },
+    });
+
+    return NextResponse.json(task);
   } catch (error) {
     console.log(error);
   }
